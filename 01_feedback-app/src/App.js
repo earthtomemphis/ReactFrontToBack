68a192549973e3ba2076d8406816f0d25aa2bdbb
Memphis DiAngelis <earthtomemphis@gmail.com>
2022-09-25 14:17:28 -0600

    feedback app

---
+++ 01_feedback-app/src/App.js
@@ -0,0 +1,20 @@
+import FeedbackForm from './components/FeedbackForm';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import Header from './components/Header';
+import { FeedbackProvider } from './context/FeedbackContext';
+
+function App() {
+	return (
+		<FeedbackProvider>
+			<Header />
+			<div className="container">
+				<FeedbackForm />
+				<FeedbackStats />
+				<FeedbackList />
+			</div>
+		</FeedbackProvider>
+	);
+}
+
+export default App;
